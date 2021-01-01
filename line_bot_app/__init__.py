5cfe3fee6e2345a165a45c68d818e1eba3cfaf1e
Gregorius Ivan Sebastian <greg.sebastian@sprintasia.co.id>
2021-01-01 15:41:19 +0700

    bug attempt #3

--- line_bot_app/__init__.py
+++ line_bot_app/__init__.py
@@ -37,3 +37,11 @@ def create_app(line_bot_api, handler):
         except InvalidSignatureError:
             abort(400)
         return 'OK'
+
+    @handler.add(MessageEvent, message=TextMessage)
+    def handle_message(event):
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=event.message.text))
+
+    return app
