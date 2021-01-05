bdcfce9b9141f61b13eee70cf9b0d625f1d51eec
Gregorius Ivan Sebastian <greg.sebastian@sprintasia.co.id>
2021-01-05 23:31:04 +0700

    adawdadwada

--- line_bot_app/responses/flex_message_responses/flex_message_templates/follow_event_flex_template.py
+++ line_bot_app/responses/flex_message_responses/flex_message_templates/follow_event_flex_template.py
@@ -40,92 +40,89 @@ def get_follow_event_flex_message(profileName="user"):
                     "color": "#e3e4e6",
                     "offsetStart": "sm"
                 },
-                # {
-                #     "type": "box",
-                #     "layout": "vertical",
-                #     "margin": "lg",
-                #     "spacing": "sm",
-                #     "contents": [
-                #         {
-                #             "type": "box",
-                #             "layout": "baseline",
-                #             "spacing": "sm",
-                #             "contents": [
-                #                 {
-                #                     "type": "text",
-                #                     "text": "Selamat datang ke Resolutions Arcade.",
-                #                     "wrap": True,
-                #                     "color": "#fb9eae",
-                #                     "size": "lg",
-                #                 }
-                #             ],
-                #             "paddingAll": "sm"
-                #         },
-                #         {
-                #             "type": "box",
-                #             "layout": "baseline",
-                #             "spacing": "sm",
-                #             "contents": [
-                #                 {
-                #                     "type": "text",
-                #                     "text": "Mainkan semua permainan yang kamu mau. Semoga kamu menikmati waktumu "
-                #                             "disini.",
-                #                     "wrap": True,
-                #                     "color": "#fb9eae",
-                #                     "size": "lg"
-                #                 }
-                #             ],
-                #             "paddingAll": "sm"
-                #         },
-                #         {
-                #             "type": "box",
-                #             "layout": "baseline",
-                #             "contents": [
-                #                 {
-                #                     "type": "text",
-                #                     "text": "Pintu kami akan selalu terbuka untukmu.",
-                #                     "wrap": True,
-                #                     "color": "#fb9eae",
-                #                     "size": "lg"
-                #                 }
-                #             ],
-                #             "margin": "sm",
-                #             "paddingAll": "sm"
-                #         },
-                #         {
-                #             "type": "box",
-                #             "layout": "baseline",
-                #             "contents": [
-                #                 {
-                #                     "type": "text",
-                #                     "text": "Sincerely, Manager Resolution Arcade",
-                #                     "wrap": True,
-                #                     "color": "#e92efb",
-                #                     "size": "lg"
-                #                 }
-                #             ],
-                #             "margin": "sm",
-                #             "paddingAll": "sm"
-                #         },
-                #         {
-                #             "type": "box",
-                #             "layout": "baseline",
-                #             "contents": [
-                #                 {
-                #                     "type": "text",
-                #                     "text": "[ @198ynrsq ]",
-                #                     "wrap": True,
-                #                     "color": "#e92efb",
-                #                     "size": "md"
-                #                 }
-                #             ],
-                #             "paddingStart": "sm"
-                #         }
-                #     ]
-                # },
-                # {
-                #     "type": "spacer"
-                # }
+                {
+                    "type": "box",
+                    "layout": "vertical",
+                    "margin": "lg",
+                    "spacing": "sm",
+                    "contents": [
+                        {
+                            "type": "box",
+                            "layout": "baseline",
+                            "spacing": "sm",
+                            "contents": [
+                                {
+                                    "type": "text",
+                                    "text": "Selamat datang ke Resolutions Arcade.",
+                                    "wrap": True,
+                                    "color": "#fb9eae",
+                                    "size": "lg",
+                                }
+                            ],
+                            "paddingAll": "sm"
+                        },
+                        {
+                            "type": "box",
+                            "layout": "baseline",
+                            "spacing": "sm",
+                            "contents": [
+                                {
+                                    "type": "text",
+                                    "text": "Mainkan semua permainan yang kamu mau. Semoga kamu menikmati waktumu "
+                                            "disini.",
+                                    "wrap": True,
+                                    "color": "#fb9eae",
+                                    "size": "lg"
+                                }
+                            ],
+                            "paddingAll": "sm"
+                        },
+                        {
+                            "type": "box",
+                            "layout": "baseline",
+                            "contents": [
+                                {
+                                    "type": "text",
+                                    "text": "Pintu kami akan selalu terbuka untukmu.",
+                                    "wrap": True,
+                                    "color": "#fb9eae",
+                                    "size": "lg"
+                                }
+                            ],
+                            "margin": "sm",
+                            "paddingAll": "sm"
+                        },
+                        {
+                            "type": "box",
+                            "layout": "baseline",
+                            "contents": [
+                                {
+                                    "type": "text",
+                                    "text": "Sincerely, Manager Resolution Arcade",
+                                    "wrap": True,
+                                    "color": "#e92efb",
+                                    "size": "lg"
+                                }
+                            ],
+                            "margin": "sm",
+                            "paddingAll": "sm"
+                        },
+                        {
+                            "type": "box",
+                            "layout": "baseline",
+                            "contents": [
+                                {
+                                    "type": "text",
+                                    "text": "[ @198ynrsq ]",
+                                    "wrap": True,
+                                    "color": "#e92efb",
+                                    "size": "md"
+                                }
+                            ],
+                            "paddingStart": "sm"
+                        }
+                    ]
+                }
             ],
             "backgroundColor": "#00284a"
         }
