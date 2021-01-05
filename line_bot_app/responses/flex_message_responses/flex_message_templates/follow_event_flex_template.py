8c8fed897021d61c69f405f31d1c96a46befe0df
Gregorius Ivan Sebastian <greg.sebastian@sprintasia.co.id>
2021-01-05 22:43:54 +0700

    last bug attempt hopefully

--- line_bot_app/responses/flex_message_responses/flex_message_templates/follow_event_flex_template.py
+++ line_bot_app/responses/flex_message_responses/flex_message_templates/follow_event_flex_template.py
@@ -4,12 +4,29 @@
 def get_follow_event_flex_message(profileName="user"):
     return {
         "type": "bubble",
+        "size": "giga",
+        "header": {
+            "type": "box",
+            "layout": "vertical",
+            "contents": [
+                {
+                    "type": "text",
+                    "text": "--Resolution Arcade--",
+                    "weight": "bold",
+                    "size": "xxl",
+                    "color": "#e3e4e6",
+                    "offsetStart": "sm",
+                    "align": "center"
+                }
+            ],
+            "backgroundColor": "#00284a"
+        },
         "hero": {
             "type": "image",
             "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
             "size": "full",
-            "aspectRatio": "20:13",
-            "aspectMode": "cover"
+            "aspectMode": "cover",
+            "aspectRatio": "20:13"
         },
         "body": {
             "type": "box",
@@ -17,10 +34,103 @@ def get_follow_event_flex_message(profileName="user"):
             "contents": [
                 {
                     "type": "text",
-                    "text": "Brown Cafe",
+                    "text": f"Hello, {profileName}!",
                     "weight": "bold",
-                    "size": "xl"
+                    "size": "xxl",
+                    "color": "#e3e4e6",
+                    "offsetStart": "sm"
+                },
+                {
+                    "type": "box",
+                    "layout": "vertical",
+                    "margin": "lg",
+                    "spacing": "sm",
+                    "contents": [
+                        {
+                            "type": "box",
+                            "layout": "baseline",
+                            "spacing": "sm",
+                            "contents": [
+                                {
+                                    "type": "text",
+                                    "text": "Selamat datang ke Resolution Arcade.",
+                                    "wrap": True,
+                                    "color": "#fb9eae",
+                                    "size": "lg",
+                                    "flex": 5
+                                }
+                            ],
+                            "paddingAll": "sm"
+                        },
+                        {
+                            "type": "box",
+                            "layout": "baseline",
+                            "spacing": "sm",
+                            "contents": [
+                                {
+                                    "type": "text",
+                                    "text": "Mainkan semua permainan yang kamu mau. Semoga kamu menikmati waktumu "
+                                            "disini.",
+                                    "wrap": True,
+                                    "color": "#fb9eae",
+                                    "size": "lg"
+                                }
+                            ],
+                            "paddingAll": "sm"
+                        },
+                        {
+                            "type": "box",
+                            "layout": "baseline",
+                            "contents": [
+                                {
+                                    "type": "text",
+                                    "text": "Pintu kami akan selalu terbuka untukmu.",
+                                    "wrap": True,
+                                    "color": "#fb9eae",
+                                    "size": "lg",
+                                    "flex": 5
+                                }
+                            ],
+                            "margin": "sm",
+                            "paddingAll": "sm"
+                        },
+                        {
+                            "type": "box",
+                            "layout": "baseline",
+                            "contents": [
+                                {
+                                    "type": "text",
+                                    "text": "Sincerely, Manager Resolution Arcade",
+                                    "wrap": True,
+                                    "color": "#e92efb",
+                                    "size": "lg",
+                                    "flex": 5
+                                }
+                            ],
+                            "margin": "sm",
+                            "paddingAll": "sm"
+                        },
+                        {
+                            "type": "box",
+                            "layout": "baseline",
+                            "contents": [
+                                {
+                                    "type": "text",
+                                    "text": "[ @198ynrsq ]",
+                                    "wrap": True,
+                                    "color": "#e92efb",
+                                    "size": "md",
+                                    "flex": 5
+                                }
+                            ],
+                            "paddingStart": "sm"
+                        }
+                    ]
+                },
+                {
+                    "type": "spacer"
                 }
-            ]
+            ],
+            "backgroundColor": "#00284a"
         }
     }
