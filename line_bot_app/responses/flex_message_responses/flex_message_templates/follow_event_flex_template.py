5fc952bb08e187d901f58a61264d1b0a38c0b834
Gregorius Ivan Sebastian <greg.sebastian@sprintasia.co.id>
2021-01-05 23:18:50 +0700

    fixxxx

--- line_bot_app/responses/flex_message_responses/flex_message_templates/follow_event_flex_template.py
+++ line_bot_app/responses/flex_message_responses/flex_message_templates/follow_event_flex_template.py
@@ -27,106 +27,106 @@ def get_follow_event_flex_message(profileName="user"):
             "size": "full",
             "aspectMode": "cover",
             "aspectRatio": "20:13"
+        },
+        "body": {
+            "type": "box",
+            "layout": "vertical",
+            "contents": [
+                # {
+                #     "type": "text",
+                #     "text": f"Hello, {profileName}!",
+                #     "weight": "bold",
+                #     "size": "xxl",
+                #     "color": "#e3e4e6",
+                #     "offsetStart": "sm"
+                # },
+                {
+                    "type": "box",
+                    "layout": "vertical",
+                    "margin": "lg",
+                    "spacing": "sm",
+                    "contents": [
+                        {
+                            "type": "box",
+                            "layout": "baseline",
+                            "spacing": "sm",
+                            "contents": [
+                                {
+                                    "type": "text",
+                                    "text": "Selamat datang ke Resolutions Arcade.",
+                                    "wrap": True,
+                                    "color": "#fb9eae",
+                                    "size": "lg",
+                                }
+                            ],
+                            "paddingAll": "sm"
+                        },
+                        {
+                            "type": "box",
+                            "layout": "baseline",
+                            "spacing": "sm",
+                            "contents": [
+                                {
+                                    "type": "text",
+                                    "text": "Mainkan semua permainan yang kamu mau. Semoga kamu menikmati waktumu "
+                                            "disini.",
+                                    "wrap": True,
+                                    "color": "#fb9eae",
+                                    "size": "lg"
+                                }
+                            ],
+                            "paddingAll": "sm"
+                        },
+                        {
+                            "type": "box",
+                            "layout": "baseline",
+                            "contents": [
+                                {
+                                    "type": "text",
+                                    "text": "Pintu kami akan selalu terbuka untukmu.",
+                                    "wrap": True,
+                                    "color": "#fb9eae",
+                                    "size": "lg"
+                                }
+                            ],
+                            "margin": "sm",
+                            "paddingAll": "sm"
+                        },
+                        {
+                            "type": "box",
+                            "layout": "baseline",
+                            "contents": [
+                                {
+                                    "type": "text",
+                                    "text": "Sincerely, Manager Resolution Arcade",
+                                    "wrap": True,
+                                    "color": "#e92efb",
+                                    "size": "lg"
+                                }
+                            ],
+                            "margin": "sm",
+                            "paddingAll": "sm"
+                        },
+                        {
+                            "type": "box",
+                            "layout": "baseline",
+                            "contents": [
+                                {
+                                    "type": "text",
+                                    "text": "[ @198ynrsq ]",
+                                    "wrap": True,
+                                    "color": "#e92efb",
+                                    "size": "md"
+                                }
+                            ],
+                            "paddingStart": "sm"
+                        }
+                    ]
+                },
+                {
+                    "type": "spacer"
+                }
+            ],
+            "backgroundColor": "#00284a"
         }
-        # "body": {
-        #     "type": "box",
-        #     "layout": "vertical",
-        #     "contents": [
-        #         {
-        #             "type": "text",
-        #             "text": f"Hello, {profileName}!",
-        #             "weight": "bold",
-        #             "size": "xxl",
-        #             "color": "#e3e4e6",
-        #             "offsetStart": "sm"
-        #         },
-        #         {
-        #             "type": "box",
-        #             "layout": "vertical",
-        #             "margin": "lg",
-        #             "spacing": "sm",
-        #             "contents": [
-        #                 {
-        #                     "type": "box",
-        #                     "layout": "baseline",
-        #                     "spacing": "sm",
-        #                     "contents": [
-        #                         {
-        #                             "type": "text",
-        #                             "text": "Selamat datang ke Resolutions Arcade.",
-        #                             "wrap": True,
-        #                             "color": "#fb9eae",
-        #                             "size": "lg",
-        #                         }
-        #                     ],
-        #                     "paddingAll": "sm"
-        #                 },
-        #                 {
-        #                     "type": "box",
-        #                     "layout": "baseline",
-        #                     "spacing": "sm",
-        #                     "contents": [
-        #                         {
-        #                             "type": "text",
-        #                             "text": "Mainkan semua permainan yang kamu mau. Semoga kamu menikmati waktumu "
-        #                                     "disini.",
-        #                             "wrap": True,
-        #                             "color": "#fb9eae",
-        #                             "size": "lg"
-        #                         }
-        #                     ],
-        #                     "paddingAll": "sm"
-        #                 },
-        #                 {
-        #                     "type": "box",
-        #                     "layout": "baseline",
-        #                     "contents": [
-        #                         {
-        #                             "type": "text",
-        #                             "text": "Pintu kami akan selalu terbuka untukmu.",
-        #                             "wrap": True,
-        #                             "color": "#fb9eae",
-        #                             "size": "lg"
-        #                         }
-        #                     ],
-        #                     "margin": "sm",
-        #                     "paddingAll": "sm"
-        #                 },
-        #                 {
-        #                     "type": "box",
-        #                     "layout": "baseline",
-        #                     "contents": [
-        #                         {
-        #                             "type": "text",
-        #                             "text": "Sincerely, Manager Resolution Arcade",
-        #                             "wrap": True,
-        #                             "color": "#e92efb",
-        #                             "size": "lg"
-        #                         }
-        #                     ],
-        #                     "margin": "sm",
-        #                     "paddingAll": "sm"
-        #                 },
-        #                 {
-        #                     "type": "box",
-        #                     "layout": "baseline",
-        #                     "contents": [
-        #                         {
-        #                             "type": "text",
-        #                             "text": "[ @198ynrsq ]",
-        #                             "wrap": True,
-        #                             "color": "#e92efb",
-        #                             "size": "md"
-        #                         }
-        #                     ],
-        #                     "paddingStart": "sm"
-        #                 }
-        #             ]
-        #         },
-        #         {
-        #             "type": "spacer"
-        #         }
-        #     ],
-        #     "backgroundColor": "#00284a"
-        # }
     }
