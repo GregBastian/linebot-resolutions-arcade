2d717a49da3a4fdc1086301ceeda51c2bccc28fd
Gregorius Ivan Sebastian <greg.sebastian@sprintasia.co.id>
2021-01-05 22:57:06 +0700

    hopefully last fix

--- line_bot_app/responses/flex_message_responses/flex_message_responses.py
+++ line_bot_app/responses/flex_message_responses/flex_message_responses.py
@@ -15,10 +15,10 @@ from line_bot_app.responses.flex_message_responses.flex_message_templates.follow
 class FlexResponses:
     def received_follow_event(self, event, line_bot_api):
         idUser = event.source.user_id
-        profileName = line_bot_api.get_profile(idUser)
+        profile = line_bot_api.get_profile(idUser)
         line_bot_api.reply_message(
             event.reply_token,
-            FlexSendMessage(alt_text=f"Welcome, {profileName}", contents=get_follow_event_flex_message(profileName))
+            FlexSendMessage(alt_text=f"Welcome, {profileName}", contents=get_follow_event_flex_message(profile.display_name))
         )
 
 flex_responses_obj = FlexResponses()
