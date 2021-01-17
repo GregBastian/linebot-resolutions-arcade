57c50c94bd29f9fe2f0f7739f471fec21523fd15
Gregorius Ivan Sebastian <greg.sebastian@sprintasia.co.id>
2021-01-18 00:36:00 +0700

    i hope this fixed the database bug

--- line_bot_app/handlers/follow_event_handler.py
+++ line_bot_app/handlers/follow_event_handler.py
@@ -17,11 +17,12 @@ class UserFollowEventHandlers:
     def user_follow_event_handler_function(self, event, line_bot_api):
         user_id = event.source.user_id
         if UserArcadeModel.user_isExist_by_user_id(user_id):
-            UserArcadeModel.reset_fields_by_user_id(user_id)
+            UserArcadeModel.add_user(user_id)
             logging.info("User does not exist")
         else:
-            UserArcadeModel.add_user(user_id)
+            UserArcadeModel.reset_fields_by_user_id(user_id)
             logging.info("User exists")
+
         flex_responses_obj.received_follow_event(event, line_bot_api)
 
 
