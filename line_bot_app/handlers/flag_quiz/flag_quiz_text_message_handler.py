5dfbec53ac1954b3c6809a87408e4c930d718470
Gregorius Ivan Sebastian <greg.sebastian@sprintasia.co.id>
2021-01-25 19:15:29 +0700

    bug fix from last time

--- line_bot_app/handlers/flag_quiz/flag_quiz_text_message_handler.py
+++ line_bot_app/handlers/flag_quiz/flag_quiz_text_message_handler.py
@@ -20,10 +20,8 @@ class FlagQuizTextMessageHandlers:
     def flag_quiz_text_message_handler_function(self, event, line_bot_api):
         message = event.message.text
         idUser = event.source.user_id
-        if UserFlagGameModel.get_game_counter_by_user_id(idUser) == FlagQuizConstants.FLAG_QUIZ_TOTAL_QUESTIONS.value:
-            flag_quiz_text_message_response_obj.get_final_score_game_when_finish(event, line_bot_api)
-
-        elif message in FlagQuizConstants.OPTIONS.value and UserFlagGameModel.get_game_counter_by_user_id(idUser) < FlagQuizConstants.FLAG_QUIZ_TOTAL_QUESTIONS.value:
+        
+        if message in FlagQuizConstants.OPTIONS.value:
             flag_quiz_flex_responses_obj.get_second_question_and_onwards(event, line_bot_api)
 
         elif message == AcceptedFlagQuizTextMessages.HELP.value:
