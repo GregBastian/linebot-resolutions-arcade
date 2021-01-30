fad1a0d6d2298e437d4db7111b66e8253bd66b04
Gregorius Ivan Sebastian <greg.sebastian@sprintasia.co.id>
2021-01-30 22:34:20 +0700

    fixed typo

--- line_bot_app/handlers/keliling_indonesia/keliling_indonesia_text_message_handler.py
+++ line_bot_app/handlers/keliling_indonesia/keliling_indonesia_text_message_handler.py
@@ -7,7 +7,7 @@ from line_bot_app.responses.keliling_indonesia_responses.text_message_responses.
     keliling_indonesia_text_message_response_obj
 
 from line_bot_app.responses.keliling_indonesia_responses.flex_message_responses.flex_message_responses import \
-    keliling_indonesiak_flex_responses_obj
+    keliling_indonesia_flex_responses_obj
 
 from line_bot_app.constants import KelilingIndonesiaConstants, AcceptedKelilingIndonesiaMessages
 
@@ -25,7 +25,7 @@ class KelilingIndonesiaTextMessageHandler:
         elif message in KelilingIndonesiaConstants.OPTIONS.value and \
                 UserKelilingIndonesiaGameModel.get_game_counter_by_user_id(
                     idUser) < KelilingIndonesiaConstants.TEBAK_BATIK_TOTAL_QUESTIONS.value:
-            keliling_indonesiak_flex_responses_obj.get_second_question_and_onwards(event, line_bot_api)
+            keliling_indonesia_flex_responses_obj.get_second_question_and_onwards(event, line_bot_api)
 
         elif message == AcceptedKelilingIndonesiaMessages.HELP.value:
             keliling_indonesia_text_message_response_obj.get_keliling_indonesia_help(event, line_bot_api)
